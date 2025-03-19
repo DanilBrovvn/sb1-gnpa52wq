9764d2646625cd3e5d9288cf5217aef0b7054244
DanilBrovvn <danil.brovvn@gmail.com>
2025-03-19 03:09:03 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -105,6 +105,7 @@ function App() {
               <div className="bg-gray-50 rounded-lg p-4">
                 <p className="text-gray-700 italic">Примеры анимаций сложных моделей</p>
                 {/* Add your video examples here */}
+                <img src="/0000-0149_1.gif" alt="Пример анимации" className="w-full rounded-lg" />
               </div>
             </div>
 
@@ -129,6 +130,7 @@ function App() {
               <div className="bg-gray-50 rounded-lg p-4">
                 <p className="text-gray-700 italic">Примеры анимаций простых моделей</p>
                 {/* Add your video examples here */}
+                <img src="/1112.gif" alt="Пример анимации" className="w-full rounded-lg" />
               </div>
             </div>
 
